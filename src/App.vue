67008eeaf2483c27e6b04f2ccd1676fc77d5c5e1
mohap710 <Mohap710@gmail.com>
2024-05-15 23:31:01 +0400

    trying to add animation to page navigations

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,24 @@
 
 <template>
   <mainnav />
-  <RouterView />
+  <router-view :key="$route.fullPath" v-slot="{ Component }">
+    <transition name="fade"> <component :is="Component" /> </transition>
+  </router-view>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .fade-enter-active {
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  .fade-leave-active {
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+  }
+
+  .fade-enter-to,
+  .fade-leave-to {
+    opacity: 1;
+  }
+</style>
