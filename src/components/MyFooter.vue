f57348eedf7309a2606a9bdc27efe3deebb24625
mohap710 <Mohap710@gmail.com>
2024-05-16 23:35:00 +0400

    add attribution footer

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by <a href="https://github.com/mohap710">Mohab Mohammed</a>.
+  </div>
+</template>
+
+<style scoped>
+  .attribution {
+    position: static;
+    background-color: var(--color-elements);
+    box-shadow: -1px 1px 2px var(--color-text);
+    padding-block: 1rem;
+    font-size: 1rem;
+    text-align: center;
+    border-radius: 30px 30px 0 0;
+  }
+  .attribution a {
+    color: #009688;
+    font-weight: 800;
+    text-decoration: none;
+  }
+</style>
